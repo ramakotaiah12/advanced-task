6cea2dd14e2582c072ec9b49fa33e50b670dd9a0
ramakotaiah12 <65582815+ramakotaiah12@users.noreply.github.com>
2021-08-12 14:43:31 +1000

    Update Description.jsx

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Description.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/Description.jsx
@@ -1,4 +1,4 @@
-﻿//changed file
+//changed file
 import React from "react";
 import Cookies from "js-cookie";
 import { Input, TextArea } from "semantic-ui-react";
@@ -6,63 +6,112 @@ import { Input, TextArea } from "semantic-ui-react";
 export class Description extends React.Component {
 	constructor(props) {
 		super(props);
-		const description =
-			props.description && props.summary
-				? Object.assign(
-						{},
-						{ description: props.description, summary: props.summary }
-				  )
-				: {
-						summary: "",
-						description: "",
-				  };
+
 		this.state = {
-			newDescription: description,
+			newDescription: "",
+			newSummary: "",
+			showEditSection: false,
 		};
-		this.update = this.update.bind(this);
-		this.saveDescriptionDetails = this.saveDescriptionDetails.bind(this);
+		this.onDescriptionChange = this.onDescriptionChange.bind(this);
+		this.onSummaryChange = this.onSummaryChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+		this.renderDisplay = this.renderDisplay.bind(this);
+		this.openEdit = this.openEdit.bind(this);
 	}
-
-	update(event) {
-		const data = Object.assign({}, this.state.newDescription);
-
-		data[event.target.name] = event.target.value;
+	openEdit() {
+		const data = Object.assign(
+			{},
+			{ summary: this.props.summary, description: this.props.description }
+		);
 		this.setState({
-			newDescription: data,
+			showEditSection: true,
+			newDescription: data.description,
+			newSummary: data.summary,
 		});
 	}
-	saveDescriptionDetails() {
-		this.props.saveProfileData(this.state.newDescription);
+	onSubmit() {
+		this.props.saveProfileData({
+			summary: this.state.newSummary,
+			description: this.state.newDescription,
+		});
+		this.setState({ showEditSection: false });
+	}
+	onDescriptionChange(e) {
+		this.setState({
+			newDescription: e.target.value,
+		});
+	}
+	onSummaryChange(e) {
+		this.setState({
+			newSummary: e.target.value,
+		});
+	}
+	renderEdit() {
+		return (
+			<div className='twenty wide column'>
+				<div className='field'>
+					<TextArea
+						autoFocus={true}
+						onChange={this.onSummaryChange}
+						value={this.state.newSummary}
+						rows={1}
+						name='summary'
+						placeholder='Please provide a short summary about yourself'
+					></TextArea>
+					<p>Summary must be no more than 150 characters</p>
+					<TextArea
+						autoFocus
+						onChange={this.onDescriptionChange}
+						value={this.state.newDescription}
+						name='description'
+						placeholder='Please tell us about any hobbies, additional expertise, or anything else you would like to add.'
+					></TextArea>
+					<p>Description must be between 150-600 characters</p>
+				</div>
+				<button
+					type='button'
+					className='ui right floated teal button'
+					onClick={this.onSubmit}
+				>
+					Save
+				</button>
+			</div>
+		);
+	}
+	renderDisplay() {
+		return (
+			<div className='twenty wide column'>
+				<div className='field'>
+					<TextArea
+						onClick={this.openEdit}
+						value={this.props.summary}
+						rows={1}
+						name='summary'
+						placeholder='Please provide a short summary about yourself'
+					></TextArea>
+					<p>Summary must be no more than 150 characters</p>
+					<TextArea
+						onClick={this.openEdit}
+						value={this.props.description}
+						name='description'
+						placeholder='Please tell us about any hobbies, additional expertise, or anything else you would like to add.'
+					></TextArea>
+					<p>Description must be between 150-600 characters</p>
+				</div>
+				<button
+					type='button'
+					className='ui right floated teal button'
+					onClick={this.onSubmit}
+				>
+					Save
+				</button>
+			</div>
+		);
 	}
 
 	render() {
-		return (
-			<React.Fragment>
-				<div className='twenty wide column'>
-					<div className='field'>
-						<TextArea
-							rows={1}
-							name='summary'
-							placeholder='Please provide a short summary about yourself'
-							onChange={(e) => this.update(e)}
-						></TextArea>
-						<p>Summary must be no more than 150 characters</p>
-						<TextArea
-							name='description'
-							placeholder='Please tell us about any hobbies, additional expertise, or anything else you would like to add.'
-							onChange={(e) => this.update(e)}
-						></TextArea>
-						<p>Description must be between 150-600 characters</p>
-					</div>
-					<button
-						type='button'
-						className='ui right floated teal button'
-						onClick={this.saveDescriptionDetails}
-					>
-						Save
-					</button>
-				</div>
-			</React.Fragment>
-		);
+		return this.state.showEditSection
+			? this.renderEdit()
+			: this.renderDisplay();
 	}
 }
